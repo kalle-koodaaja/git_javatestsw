ff6fd94fa86179773a52e1d41710ed9f564daf9c
Mika Nokka <mika.nokka@ambientia.fi>
2015-09-03 14:35:00 +0300

    COde change for JIRA task: MN-7 - MN Test Project Task

--- src/main/java/com/ambientia/app/App.java
+++ src/main/java/com/ambientia/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! - Hyvvee paevee" );
+        System.out.println( "Hello World! - Hyvvee paevee - Goden dag" );
     }
 }
