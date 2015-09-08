e02ec29a6dacd475a1acbb8f0b14e0e0f7f9b7a8
Mika Nokka <mika.nokka@ambientia.fi>
2015-09-08 16:27:48 +0300

    Code change for JIRA task: SWDEV-2 - Java test SW to echo date

--- src/main/java/com/ambientia/app/App.java
+++ src/main/java/com/ambientia/app/App.java
@@ -1,5 +1,7 @@
 package com.ambientia.app;
 
+import java.util.Date;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
+		Date date = new Date();
         System.out.println( "Hello World! - Hyvvee paevee - Goden dag" );
+		System.out.println( "--------------date is----------------------" );
+		System.out.println(date.toString());
     }
 }
