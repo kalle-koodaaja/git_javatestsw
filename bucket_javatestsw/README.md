ac6140f5eb8943df4b8e11424b91e15ac85ebd38
Kalle Koodaaja <kalle_koodaaja@ambientia.fi>
2015-09-22 10:38:19 +0300

    hellotest moved under bucket_javatestsw

---
+++ bucket_javatestsw/README.md
@@ -0,0 +1,10 @@
+Kalle Koodaaja presents:
+
+Simple Java test SW for Ambientia ALMDemo purposes - original implementation by mnokka 
+
+
+* Manual building using Maven
+-info tbd
+
+* Execute via console
+-info tbd ..
---
+++ bucket_javatestsw/src/main/java/com/ambientia/app/App.java
@@ -0,0 +1,18 @@
+package com.ambientia.app;
+
+import java.util.Date;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+		Date date = new Date();
+        System.out.println( "Hello World! - Hyvvee paevee - Goden dag" );
+		System.out.println( "--------------date is----------------------" );
+		System.out.println(date.toString());
+    }
+}
